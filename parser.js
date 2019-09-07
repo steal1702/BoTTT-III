7cdf44b81cef8ef8ee94bcb82a1ea1e0fee65e7b
Leonard Craft <leonardcraft64@gmail.com>
2019-09-07 14:10:52 -0500

    Update autocorrect regex for Amoonguss

--- parser.js
+++ parser.js
@@ -604,7 +604,7 @@ exports.parse =
 			}
 
 			//Autocorrect regexes and corresponding autocorrections
-			let autocorrectRegexes = [/(n|N)inetails/, /(m|M)eowstick/, /(c|C)owmoo/, /tylee/, /(w|W)olfie/, /(A|a)moongus([^s]|$)/, /(s|S)hedninja/, /(d|d)a(w|W)obblefet/];
+			let autocorrectRegexes = [/(n|N)inetails/, /(m|M)eowstick/, /(c|C)owmoo/, /tylee/, /(w|W)olfie/, /(A|a)m(o|oo)ngus([^s]|$)/, /(s|S)hedninja/, /(d|d)a(w|W)obblefet/];
 			let autocorrectMessages = ["Ninetales", "Meowstic", "Kommo-o", "tlyee", "Wolfey", "Amoonguss", "Shedinja", "DaWoblefet"];
 
 			for (let i = 0; i < autocorrectRegexes.length; i++)
